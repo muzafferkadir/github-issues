7f5df8bc00f6fd2c276d8fdb21b7da827d065cca
muzafferkadir <muzafferkadiryilmaz@gmail.com>
2024-05-05 19:10:04 +0300

    fix: readme file and type fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,19 @@
 import IssuesTable from "@/components/issuesTable/IssuesTable";
-import {
-  getIssuesData,
-  getTotalCounts,
-  totalCountsResponse,
-  getLabels,
-} from "@/services/api";
-import { Issue, Label } from "@/types";
+import { getIssuesData, getTotalCounts, getLabels } from "@/services/api";
+import { Issue, Label, totalCountsResponse } from "@/types";
 
 export default async function Home() {
   const counts = (await getTotalCounts()) as totalCountsResponse;
   const { items: issues } = (await getIssuesData()) as { items: Issue[] };
-  const labels = await getLabels() as Label[];
+  const labels = (await getLabels()) as Label[];
 
   return (
     <main className="flex flex-wrap -mx-2 gap-y-4 lg:gap-y-0">
-      <IssuesTable totalCounts={counts} initialIssues={issues} labels={labels} />
+      <IssuesTable
+        totalCounts={counts}
+        initialIssues={issues}
+        labels={labels}
+      />
     </main>
   );
 }
